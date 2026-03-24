4a40dc98a9b52b0e7b3f86c7c7d766dc68fa536c
Suresh Kumar <hgsuresh62@gmail.com>
2026-03-24 19:56:21 +0530

    Fix render port issue

--- app.py
+++ app.py
@@ -835,7 +835,5 @@ def logout():
 import os
 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 8000))
-
-app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
